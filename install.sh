78cfc4cbc64ac34b35aa1fe3162defa912d2578d
nikp123 <nikp123@e.email>
2024-06-25 23:45:05 +0200

    Some fixes :/

--- install.sh
+++ install.sh
@@ -12,27 +12,27 @@ which convert >/dev/null 2>&1
 # Copy font config
 cp -v ./font/config/* ${FONTCONFIG_PATH}
 # Copy font
-cp -v ./font/Minecraft.otf /usr/share/fonts/OTF/
+install -v -d -m 0755 /usr/share/fonts/OTF/
+install -v -m 0644 ./font/Minecraft.otf /usr/share/fonts/OTF/
 
 # Copy plymouth theme
-mkdir ${PLYMOUTH_THEME_BASEDIR}
-cp -vr ./plymouth/mc.script ${PLYMOUTH_THEME_BASEDIR}
-cp -vr ./plymouth/mc.plymouth ${PLYMOUTH_THEME_BASEDIR}
-cp -vr ./plymouth/progress_bar.png ${PLYMOUTH_THEME_BASEDIR}
-cp -vr ./plymouth/progress_box.png ${PLYMOUTH_THEME_BASEDIR}
+install -v -d -m 0755 ${PLYMOUTH_THEME_BASEDIR}
+install -v -m 0644 ./plymouth/mc.script ${PLYMOUTH_THEME_BASEDIR}
+install -v -m 0644 ./plymouth/mc.plymouth ${PLYMOUTH_THEME_BASEDIR}
+install -v -m 0644 ./plymouth/progress_bar.png ${PLYMOUTH_THEME_BASEDIR}
+install -v -m 0644 ./plymouth/progress_box.png ${PLYMOUTH_THEME_BASEDIR}
 
 # Create smaller versions of assets
 for j in "padlock" "bar"; do
-	mkdir ${PLYMOUTH_THEME_BASEDIR}/${j}
 	for i in $(seq 1 6); do
-		convert ./plymouth/${j}.png -interpolate Nearest -filter point -resize "$i"00% ${PLYMOUTH_THEME_BASEDIR}/${j}/"${i}".png
+		convert ./plymouth/${j}.png -interpolate Nearest -filter point -resize "$i"00% ${PLYMOUTH_THEME_BASEDIR}/${j}-"${i}".png
 	done
 done	
 
-mkdir ${PLYMOUTH_THEME_BASEDIR}/dirt
 for i in $(seq 1 12); do
-	convert -channel R -evaluate multiply .2509803922 -channel G -evaluate multiply .2509803922 -channel B -evaluate multiply .2509803922 ./plymouth/dirt.png -interpolate Nearest -filter point -resize "$i"00% ${PLYMOUTH_THEME_BASEDIR}/dirt/"${i}".png
+	magick ./plymouth/dirt.png -channel R -evaluate multiply .2509803922 -channel G -evaluate multiply .2509803922 -channel B -evaluate multiply .2509803922 -interpolate Nearest -filter point -resize "$i"00% ${PLYMOUTH_THEME_BASEDIR}/dirt-"${i}".png
 done
 
 # Install dracut config
-cp -v ./dracut/* /etc/dracut.conf.d/99-minecraft-plymouth.conf
+install -v -d -m 0755 /etc/dracut.conf.d
+install -v -m 0644 ./dracut/* /etc/dracut.conf.d/99-minecraft-plymouth.conf
